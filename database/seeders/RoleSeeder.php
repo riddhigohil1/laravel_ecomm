722e2873993ed84abaaacc9f221046f6f1237bd8
Riddhi G <riddhig@mac.mynetworksettings.com>
2025-11-12 14:07:56 -0500

    Laravel/React starter kit with SPATIE role and permission

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\PermissionsEnum;
+use App\Enum\RolesEnum;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userRole = Role::create(["name"=> RolesEnum::User->value]);
+        $adminRole = Role::create(["name"=> RolesEnum::Admin->value]);
+        $vendorRole = Role::create(["name"=> RolesEnum::Vendor->value]);
+
+        $approveVendor = Permission::create(["name"=> PermissionsEnum::ApproveVendors->value]);
+        $buyProducts = Permission::create(["name"=> PermissionsEnum::BuyProducts->value]);
+        $sellProducts = Permission::create(["name"=> PermissionsEnum::SellProducts->value]);
+
+        $userRole->syncPermissions( $buyProducts);
+        $vendorRole->syncPermissions( $sellProducts, $buyProducts);
+        $adminRole->syncPermissions( $approveVendor, $buyProducts, $sellProducts);
+    }
+}
