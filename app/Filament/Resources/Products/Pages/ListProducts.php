18c0efa08722bac630a25467cdfdcc96492c590c
Riddhi G <riddhig@mac.mynetworksettings.com>
2025-12-06 09:41:12 -0500

    Added Conversation for Image

--- app/Filament/Resources/Products/Pages/ListProducts.php
+++ app/Filament/Resources/Products/Pages/ListProducts.php
@@ -14,6 +14,7 @@ class ListProducts extends ListRecords
     {
         return [
             CreateAction::make(),
+            
         ];
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,11 +7,24 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Product extends Model implements HasMedia
 {
     use InteractsWithMedia;
-    
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(100);
+
+        $this->addMediaConversion('small')
+            ->width(480);
+
+        $this->addMediaConversion('large')
+            ->width(1200);
+    }
+
     public function department(): BelongsTo
     {
         return $this->belongsTo(Department::class);
