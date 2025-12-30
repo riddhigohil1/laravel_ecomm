d1a4bd75b4a92555bbe787fd20187b735187a1e5
Riddhi G <riddhig@mac.mynetworksettings.com>
2025-12-30 11:25:41 -0500

    Bug Fix

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/layouts/Footer';
-import Header from '@/layouts/header';
+import Header from '@/layouts/Header';
 import { PaginationProps, Product } from '@/types';
 import ProductList from './ProductList';
 
